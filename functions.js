b19455eb1844d551b8c8fdde0292b910aea90621
martinorue <martin.orue008@gmail.com>
2023-01-17 17:22:36 -0300

    add states

---
+++ functions.js
@@ -0,0 +1,26 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const ratingButtons = document.querySelectorAll('.rating-button');
+    console.log(ratingButtons)
+    ratingButtons.forEach(button => button.addEventListener('click', handleClick));
+
+    function handleClick(event) {
+        console.log('holis')
+        ratingButtons.forEach(button => button.classList.remove('active'));
+        event.target.classList.add('active');
+    }
+
+    const submitButton = document.querySelector('.rating-state__submit');
+    submitButton.addEventListener('click', handleSubmitClick);
+
+    function handleSubmitClick(event) {
+        event.preventDefault();
+        console.log('holiii');
+        const ratingState = document.querySelector('.rating-state');
+        ratingState.classList.add('hide');
+        const thankState = document.querySelector('.thank-state');
+        thankState.classList.remove('hide');
+    }
+
+});
+
